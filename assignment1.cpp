d8dc8e4711c69e2e17e28cfe72f907d43d7e2840
Ishan Joshi <ishanjoshi02@gmail.com>
2017-01-20 19:00:06 +0530

    Completed Assignment 1

--- assignment1.cpp
+++ assignment1.cpp
@@ -137,6 +137,31 @@ int precedence(char c) {
 	return 3;
 }
 
+string reverseString(string expression) {
+
+	int length = expression.length();
+	string reverse = "";
+	char c;
+	Stack<char> stack;
+
+	for(int i = 0; i<length;i++) {
+		c = expression.at(i);
+		stack.push(c);
+	}
+
+	while(!stack.isEmpty()) {
+		c = stack.pop();
+		if (c == ')') {
+			c = '(';
+		}
+		else if (c == '(') {
+			c = ')';
+		}
+		reverse += c;
+	}
+	return reverse;
+}
+
 string toPostFix(string expression) {
 
 	char c, op;
@@ -150,24 +175,85 @@ string toPostFix(string expression) {
 		c = expression.at(i);
 		if(isalnum(c)) {
 
-			postFixExpression.append(1,c);
+			postFixExpression += c;
 
 		}
-		else if (c == '(') {
-			ExpressionStack.push(c);
+		else {
+			if (c == '(') {
+				ExpressionStack.push(c);
+			}
+			else if(c == ')') {
+				while ((op = ExpressionStack.pop())!='(') {
+					postFixExpression += op;
+				}
+			}
+
+			else if(!ExpressionStack.isEmpty() && precedence(ExpressionStack.stackTop())>= precedence(c)) {
+
+				postFixExpression += ExpressionStack.pop();
+
+			}
+			if(c!='(' && c!=')') {
+				ExpressionStack.push(c);
+			}
 		}
-		else if((op = ExpressionStack.pop())!=')') {
-			postFixExpression.append(1,op);
-		}
+
+
+	}
+	while (!ExpressionStack.isEmpty()) {
+
+		postFixExpression += ExpressionStack.pop();
 	}
 
 	return postFixExpression;
 
 }
 
+float postFixEvaluation(string expression) {
+
+	int length = expression.length();
+	float answer, digit;
+	char c;
+	Stack<float> integers;
+	for(int i =0; i<length;i++) {
+		c = expression.at(i);
+		if(isalnum(c)) {
+			if(c>47 && c <= 57) {
+				digit = c - '0';
+			}
+			else {
+				cout << "Enter value for " + c << endl;
+				cin >>digit;
+			}
+			integers.push(digit);
+		}
+		else {
+			if (c == '+') {
+				answer = integers.pop() + integers.pop();
+			}
+			else if (c == '-') {
+				answer = integers.pop() - integers.pop();
+			}
+			else if(c == '*') {
+				answer = integers.pop() * integers.pop();
+			}
+			else if(c == '/') {
+				float some = integers.pop();
+				answer =  integers.pop()/some;
+			}
+			integers.push(answer);
+		}
+	}
+	return integers.pop();
+}
+
+int preFixEvaluation(string expression) {
+
+}
+
 int main(int argc, char **argv) {
 
-	Stack<int> IntegerStack;
+	/*Stack<int> IntegerStack;
 	Stack<string> StringStack;
 
 	int option;
@@ -255,7 +341,25 @@ int main(int argc, char **argv) {
 			}
 		}
 		}
-	} while (true);
+	} while (true);*/
+
+	//Infix to Postfix
+	/*string Expression;
+	cout << "Enter an Expression\n";
+	cin >> Expression;
+	cout << toPostFix(Expression);*/
+
+	//Infix to Prefix
+	/*string Expression;
+			cout << "Enter an Expression\n";
+			cin >> Expression;
+			cout << reverseString(toPostFix());*/
+//PostFix Evaluation
+	/*string Expression;
+	cout << "Enter an Expression\n";
+	cin >> Expression;
+	cout << postFixEvaluation(toPostFix(Expression)) << endl;
+*/
 }
 
 
